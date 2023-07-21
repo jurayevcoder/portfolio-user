8fbbc39e3cae0529995d9b6f6e406b2ae4e37b29
jurayevcoder <davlatjurayev.1@gmail.com>
2023-07-21 19:35:41 +0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <Header />
+        <main class="min-h-screen bg-[#313131]">
+            <slot />
+        </main>
+    </div>
+</template>
+  
+<script setup></script>
+  
+<style lang="scss" scoped></style>
