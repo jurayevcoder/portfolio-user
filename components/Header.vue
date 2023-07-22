d77a6ac503bc718778cd571196a969f2fcb3177f
jurayevcoder <davlatjurayev.1@gmail.com>
2023-07-22 18:54:52 +0500

    header add

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,62 @@
-<template></template>
+<template>
+    <header class="bg-[#414141] fixed z-50 top-0 w-full border-b border-white">
+        <div class="container mx-auto px-5">
+            <nav class="w-full flex items-center justify-between py-5">
+                <div>
+                    <a href="#" class="flex items-center">
+                        <h1 class="text-[24px] text-[#FF4F00] font-bold">Jurayev</h1> <span
+                            class="text-[24px] text-white font-bold">Coder</span>
+                    </a>
+                </div>
+                <div class="hidden md:block">
+                    <ul class="flex items-center gap-8 text-[16px] text-white font-bold">
+                        <li class="border-b p-2 border-[#414141] hover:border-[#FF4F00]">
+                            <NuxtLink to="/">Home</NuxtLink>
+                        </li>
+                        <li class="border-b p-2 border-[#414141] hover:border-[#FF4F00]">
+                            <a href="#">About</a>
+                        </li>
+                        <li class="border-b p-2 border-[#414141] hover:border-[#FF4F00]">
+                            <a href="#">Portfolio</a>
+                        </li>
+                        <li class="border-b p-2 border-[#414141] hover:border-[#FF4F00]">
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="md:hidden reletive">
+                    <button @click="toggleMenu" class="flex p-1 rounded-lg focus:ring-2 focus:ring-[#313131]">
+                        <i class='text-[24px] text-white font-bold' :class="menu ? 'bx bx-x' : 'bx bx-menu'"></i>
+                    </button>
+                    <ul class="absolute bg-[#414141] w-full  left-0 top-[77px] text-white font-bold text-center text-[18px]"
+                        :class="menu ? 'bloc' : 'hidden'">
+                        <li class="border-b p-2 border-[#525252] hover:border-[#FF4F00] pl-5 mb-5 mt-5">
+                            <NuxtLink to="/">Home</NuxtLink>
+                        </li>
+                        <li class="border-b p-2 border-[#525252] hover:border-[#FF4F00] pl-5 mb-5">
+                            <a href="#">About</a>
+                        </li>
+                        <li class="border-b p-2 border-[#525252] hover:border-[#FF4F00] pl-5 mb-5">
+                            <a href="#">Portfolio</a>
+                        </li>
+                        <li class="border-b p-2 border-[#525252] hover:border-[#FF4F00] pl-5">
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
 
 <script setup>
-
+import { ref } from '../.nuxt/imports';
+const menu = ref(false);
+const toggleMenu = () => (menu.value = !menu.value);
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.router-link-exact-active {
+    color: #FF4F00;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-      '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss'
   ],
   app: {
-      head: {
-        title: "JurayevCoder",
-        link: [
-          {
-            rel:"preconnect",
-            href:"https://fonts.googleapis.com"
-          },
-          {
-            rel:"preconnect",
-            href:"https://fonts.gstatic.com",
-          },
-          {
-            rel:"stylesheet",
-            href:"https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          },
-          {
-            rel: "icon",
-            type: "image/x-icon",
-            href: "/favicon.png"
-          },
-          {
-            rel: "stylesheet",
-            href: "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css",
-          },
-        ],
-        meta: [{ name: "author", content: "Davlat Jo'rayev - jurayevcoder" }],
-      },
+    head: {
+      title: "JurayevCoder",
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com"
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+        },
+        {
+          rel: "icon",
+          type: "image/png",  
+          href: "/favicon.png"
+        },
+        {
+          rel: "stylesheet",
+          href: "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css",
+        },
+      ],
+      meta: [{ name: "author", content: "JurayevCoder" }],
     },
+  },
 })
